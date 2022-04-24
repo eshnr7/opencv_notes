940c7ee4ea6b4d6404b08a4cdced8453e07df8c0
Enes Şahiner <eshnr7@gmail.com>
2022-04-24 16:45:24 +0300

    reading, showing, and saving an image
    to read an image saved on the computer

---
+++ reading an image/open_pic.py
@@ -0,0 +1,9 @@
+import cv2
+
+import numpy as np
+
+image = cv2.imread("pic_1.jpg", 0) # if zero added at the end, the image will be black and white. Thus in this code, relevant image will be black and white
+cv2.imshow("title", image)
+# cv2.imwrite("new_name_of_the_image") to save as a new image
+cv2.waitKey(0)
+cv2.destroyAllWindows()
